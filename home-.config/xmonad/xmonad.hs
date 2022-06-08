a1f43bb3b58db50da552ff4c140189bd9ed190e3
Landon <102832670+HktOverload@users.noreply.github.com>
2022-06-08 16:24:00 -0400

    Create xmonad.hs
    I've only had this WM for a day please don't shame me for doing so little (even though I already now Haskell decently well).

---
+++ home-.config/xmonad/xmonad.hs
@@ -0,0 +1,24 @@
+import XMonad
+
+import XMonad.Util.Run
+import XMonad.Util.EZConfig
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+
+customKeys =
+	[ ( (0, 0xFFEB), spawn "dmenu_apps" )
+	, ( (0, 0xFF61), spawn "scrot_kt" )
+	]
+
+main :: IO ()
+main = do
+    xmobarPipe <- spawnPipe "xmobar"
+    xmonad $ def
+    	{ terminal = "sakura"
+    	, manageHook = manageDocks <+> manageHook def
+    	, layoutHook = avoidStruts $ layoutHook def
+    	, handleEventHook = handleEventHook def <+> docksEventHook
+        , logHook = dynamicLogWithPP $ xmobarPP
+    		{ ppOutput = hPutStrLn xmobarPipe
+    		}
+    	} `additionalKeys` customKeys
