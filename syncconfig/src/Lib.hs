9d34768996b8872c215b029f4c893c26b27e5121
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-06-25 16:36:08 -0400

    desync handling

--- syncconfig/src/Lib.hs
+++ syncconfig/src/Lib.hs
@@ -3,5 +3,20 @@ module Lib
     ) where
 
 checkSync :: (String, String) -> IO ()
-checkSync (repo, fs) = putStrLn $ repo ++ " <--?--> " ++ fs
+checkSync (repo, fs) =
+    let isDesync = liftM2 (/=) (readFile repo) (readFile fs) in
+    isDesync >>= (\x ->
+        if x then handleDesync (repo, fs) else handleSync fs
+    )
+
+handleDesync :: (String, String) -> IO ()
+handleDesync (repo, fs) = do
+    putStrLn "[DESYNC] repo:" ++ repo ++ " differs from fs:" ++ fs
+    putStr "[DESYNC] Should I [i]gnore this, update (r)epo, or update (f)s? "
+    resp <- getLine
+    case bool resp "i" $ (==[]) resp of
+        "i" -> putStrLn "[DESYNC] Ignored " ++ fs ++ "."
+	"r" -> updateRepo (repo, fs)
+        "f" -> updateFs (repo, fs)
+	_ -> putStrLn "[DESYNC] Invalid option. Ignored " ++ fs ++ "."
 
