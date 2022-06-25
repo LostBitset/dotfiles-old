e18ca1d14ff479508cfc906928e648a02c4f6a90
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-06-25 16:43:48 -0400

    i forgot how precedence works lol

--- syncconfig/src/Lib.hs
+++ syncconfig/src/Lib.hs
@@ -14,21 +14,21 @@ checkSync (repo, fs) =
 
 handleDesync :: (String, String) -> IO ()
 handleDesync (repo, fs) = do
-    putStrLn "[DESYNC] repo:" ++ repo ++ " differs from fs:" ++ fs
-    putStr "[DESYNC] Should I [i]gnore this, update (r)epo, or update (f)s? "
+    putStrLn $ "[DESYNC] repo:" ++ repo ++ " differs from fs:" ++ fs
+    putStr $ "[DESYNC] Should I [i]gnore this, update (r)epo, or update (f)s? "
     resp <- getLine
     case bool resp "i" $ (==[]) resp of
-        "i" -> putStrLn "[DESYNC] Ignored " ++ fs ++ "."
+        "i" -> putStrLn $ "[DESYNC] Ignored " ++ fs ++ "."
 	"r" -> updateRepo (repo, fs)
         "f" -> updateFs (repo, fs)
-	_ -> putStrLn "[DESYNC] Invalid option. Ignored " ++ fs ++ "."
+	_ -> putStrLn $ "[DESYNC] Invalid option. Ignored " ++ fs ++ "."
 
 handleSync :: String -> IO ()
-handleSync fs = putStrLn "[OK]    File " ++ fs ++ " is synchronized."
+handleSync fs = putStrLn $ "[OK]    File " ++ fs ++ " is synchronized."
 
 updateRepo :: (String, String) -> IO ()
-updateRepo (repo, fs) = putStrLn "!! <dbg> repo:" ++ repo ++ " <- fs:" ++ fs
+updateRepo (repo, fs) = putStrLn $ "!! <dbg> repo:" ++ repo ++ " <- fs:" ++ fs
 
 updateFs :: (String, String) -> IO ()
-updateFs (repo, fs) = putStrLn "!! <dbg> fs:" ++ fs ++ " <- repo:" ++ repo
+updateFs (repo, fs) = putStrLn $ "!! <dbg> fs:" ++ fs ++ " <- repo:" ++ repo
 
