077b9bae8679f310dab64bd230d4d63d79a273c1
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-06-25 20:21:45 -0400

    auto

--- syncconfig/src/Lib.hs
+++ syncconfig/src/Lib.hs
@@ -4,44 +4,56 @@ module Lib
 
 import Control.Monad (liftM2, (<=<))
 import Data.Bool (bool)
+import System.Directory (copyFile)
 import System.Environment (getEnv)
+import System.IO (hFlush, stdout)
+import System.Process (callProcess)
 
 checkSyncRaw :: (String, String) -> IO ()
 checkSyncRaw = checkSync <=< fixRawPaths
 
-fixRawPaths :: (String, String) -> IO (String, String)
+fixRawPaths :: (String, String) -> IO (FilePath, FilePath)
 fixRawPaths (repoRaw, fsRaw) =
     let givenHome = \home -> ("../" ++ repoRaw, replHome fsRaw home) in
     givenHome <$> getEnv "HOME"
 
-replHome :: String -> String -> String
+replHome :: FilePath -> FilePath -> FilePath
 replHome ('~':xs) = (++xs)
 replHome s@_ = const s
 
-checkSync :: (String, String) -> IO ()
+checkSync :: (FilePath, FilePath) -> IO ()
 checkSync (repo, fs) =
     let isDesync = liftM2 (/=) (readFile repo) (readFile fs) in
     isDesync >>= (\x ->
         if x then handleDesync (repo, fs) else handleSync fs
     )
 
-handleDesync :: (String, String) -> IO ()
+handleDesync :: (FilePath, FilePath) -> IO ()
 handleDesync (repo, fs) = do
     putStrLn $ "[DESYNC] repo:" ++ repo ++ " differs from fs:" ++ fs
     putStr $ "[DESYNC] Should I [i]gnore this, update (r)epo, or update (f)s? "
+    hFlush stdout
     resp <- getLine
-    case bool resp "i" $ (==[]) resp of
+    case bool resp "i" $ null resp of
         "i" -> putStrLn $ "[DESYNC] Ignored " ++ fs ++ "."
         "r" -> updateRepo (repo, fs)
         "f" -> updateFs (repo, fs)
         _ -> putStrLn $ "[DESYNC] Invalid option. Ignored " ++ fs ++ "."
 
-handleSync :: String -> IO ()
+handleSync :: FilePath -> IO ()
 handleSync fs = putStrLn $ "[OK]    File " ++ fs ++ " is synchronized."
 
-updateRepo :: (String, String) -> IO ()
-updateRepo (repo, fs) = putStrLn $ "!! <dbg> repo:" ++ repo ++ " <- fs:" ++ fs
+updateRepo :: (FilePath, FilePath) -> IO ()
+updateRepo (repo, fs) = (fs `copyFile` repo) *> syncRepo
 
-updateFs :: (String, String) -> IO ()
-updateFs (repo, fs) = putStrLn $ "!! <dbg> fs:" ++ fs ++ " <- repo:" ++ repo
+syncRepo :: IO ()
+syncRepo = (++"/bin/gitw") <$> getEnv "HOME" >>= flip callProcess ["auto"]
+
+updateFs :: (FilePath, FilePath) -> IO ()
+updateFs (repo, fs) = do
+    putStrLn $ "[DESYNC] Warning: This will overwrite " ++ fs ++ " forever!"
+    putStr "[DESYNC] Are you sure about this? ([no]/destroy): "
+    hFlush stdout
+    resp <- getLine
+    if resp == "destroy" then repo `copyFile` fs else return ()
 
