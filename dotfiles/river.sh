4e614de0db3b5308f5dffcc8c22fed6aaafd9e0e
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-10-06 17:25:07 -0400

    more river configuration

--- dotfiles/river.sh
+++ dotfiles/river.sh
@@ -9,6 +9,27 @@ riverctl map normal Mod4 Q spawn "$WEZTERM_INSTALL"
 riverctl map normal Mod4 W spawn firefox
 
 riverctl map normal Mod4 U close
+riverctl map normal Mod4 H focus-view previous
+riverctl map normal Mod4 L focus-view next
+riverctl map normal Mod4 B zoom
+
+riverctl declare-mode layout-ctrl
+riverctl map normal Mod4 Y enter-mode layout-ctrl
+riverctl map layout-ctrl Mod4 Y enter-mode normal
+
+riverctl map layout-ctrl None H send-layout-cmd rivertile "main-ratio -0.05"
+riverctl map layout-ctrl None J send-layout-cmd rivertile "main-count -1"
+riverctl map layout-ctrl None K send-layout-cmd rivertile "main-count +1"
+riverctl map layout-ctrl None L send-layout-cmd rivertile "main-ratio +0.05"
+
+riverctl map layout-ctrl None Up send-layout-cmd rivertile "main-location top"
+riverctl map layout-ctrl None Down send-layout-cmd rivertile "main-location bottom"
+riverctl map layout-ctrl None Left send-layout-cmd rivertile "main-location left"
+riverctl map layout-ctrl None Right send-layout-cmd rivertile "main-location right"
+
+riverctl declare-mode passthrough
+riverctl map normal Super A enter-mode passthrough
+riverctl map passthrough Super A enter-mode normal
 
 riverctl map normal Mod4+Shift V exit
 
