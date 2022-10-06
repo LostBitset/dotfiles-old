7853e8c231b8361d5db3cc8d00a05cff54e425ae
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-10-06 17:10:17 -0400

    add `rc.local` dotfile

---
+++ dotfiles/rc-local.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create a directory in /run/user for each user
+# This is designed to be used as their $XDG_RUNTIME_DIR
+cat /etc/passwd | awk -F ':' '{print $1}' | while read u; do
+	dir="/run/user/$u"
+	rm -rf $dir || true
+	mkdir -p $dir
+	chmod 700 $dir
+	chown $u $dir
+done
+
