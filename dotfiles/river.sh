c4848e5e29799e69d5cc94268b80a281c26bb289
LostBitset <102832670+LostBitset@users.noreply.github.com>
2022-10-28 16:06:06 -0400

    no more swww

--- dotfiles/river.sh
+++ dotfiles/river.sh
@@ -63,11 +63,6 @@ riverctl set-repeat 50 300
 # PulseAudio
 riverctl spawn 'pulseaudio --realtime --start'
 
-# swww for wallpaper
-swww kill # Kill the daemon if it previously existed
-riverctl spawn 'swww init'
-riverctl spawn 'swww img $BG_INTENT'
-
 # I use yambar
 riverctl spawn 'yambar'
 
